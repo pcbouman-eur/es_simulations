cf5671494fd3d0e36a30cc66609095c6c5ade7f2
mateuszwilinski <mateuszwilinski@sns.it>
2020-01-28 20:09:54 +0100

    added the standard sbm

--- net_generation/base.py
+++ net_generation/base.py
@@ -2,12 +2,28 @@ import igraph as ig
 import numpy as np
 
 
-def init_sbm(n, af_matrix):
+def init_sbm(n, affinity):
     """
 
     :param n: network size
-    :param af_matrix: affinity matrix for SBM model
+    :param affinity: affinity matrix for SBM model
     :return: ig.Graph() object with...
     """
-    g = ig.Graph()
+    q = len(affinity)
+    block_sizes = np.repeat(n / q, q).tolist()
+    g = ig.Graph.SBM(n, affinity, block_sizes)
     g.vs()["state"] = np.random.randint(0, 2, n) * 2 - 1
+
+    group = np.zeros(n, dtype='int')
+    for i in range(q - 1):
+        group[int(np.sum(block_sizes[:(i + 1)])):] = i + 1
+    g.vs['district'] = group
+    return g
+
+
+if __name__ == '__main__':
+    affinity = [[0.2, 0.01], [0.01, 0.2]]
+    g = init_sbm(100, affinity)
+    colors = np.array(['blue', 'green'])
+    g.vs['color'] = colors[np.array(g.vs['district']) - 1]
+    ig.plot(g)
