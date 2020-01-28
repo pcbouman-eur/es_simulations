64a8b227acd5d934c16695dac487cf60753e7617
tomaszraducha <raducha.tomasz@gmail.com>
2020-01-28 19:43:44 +0100

    generating func scratch

--- net_generation/base.py
+++ net_generation/base.py
@@ -1,1 +1,13 @@
 import igraph as ig
+import numpy as np
+
+
+def init_sbm(n, af_matrix):
+    """
+
+    :param n: network size
+    :param af_matrix: affinity matrix for SBM model
+    :return: ig.Graph() object with...
+    """
+    g = ig.Graph()
+    g.vs()["state"] = np.random.randint(0, 2, n) * 2 - 1
