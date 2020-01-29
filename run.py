156ddb399e9aba2e0076d33e2b9aeba01b6cf1c6
mateuszwilinski <mateuszwilinski@sns.it>
2020-01-30 00:36:43 +0100

    added function for zealots

--- run.py
+++ run.py
@@ -5,8 +5,8 @@ from net_generation.base import init_sbm, add_zealots, planted_affinity
 from simulation.base import run_symulation
 from electoral_sys.electoral_system import system_population_majority, system_district_majority
 
-N = 5000  # network size
-q = 100
+N = 1000  # network size
+q = 20
 # AFFINITY = [[0.2, 0.01], [0.01, 0.2]]  # change to get different network from SBM
 AFFINITY = planted_affinity(q, 5, np.ones(q) / q, 0.2, N)  # all districts the same size and density
 EPS = 0.01  # noise rate
@@ -17,12 +17,14 @@ def plot_hist(distribution):
     plt.title('Histogram of vote share')
     plt.xlabel('fraction of votes')
     plt.ylabel('probability')
+    plt.savefig('../../plot1.png')
     plt.show()
 
     plt.hist(distribution[-1], bins=10, normed=True, color='red')
     plt.title('Histogram of vote share')
     plt.xlabel('fraction of votes')
     plt.ylabel('probability')
+    plt.savefig('../../plot2.png')
     plt.show()
 
 
@@ -31,11 +33,11 @@ def main():
     dist_district_wise = {1: [], -1: []}
 
     init_g = init_sbm(N, AFFINITY)
-    init_g = add_zealots(init_g)
-    g = run_symulation(init_g, EPS, 100000, n=N)
+    init_g = add_zealots(init_g, 0)
+    g = run_symulation(init_g, EPS, 200000, n=N)
 
-    for i in range(500):
-        g = run_symulation(g, EPS, N*10, n=N)
+    for i in range(1000):
+        g = run_symulation(g, EPS, N*40, n=N)
         dist_population_wise[1].append(system_population_majority(g.vs)['fractions'][1])
         dist_population_wise[-1].append(system_population_majority(g.vs)['fractions'][-1])
         dist_district_wise[1].append(system_district_majority(g.vs)['fractions'][1])
@@ -48,10 +50,10 @@ def main():
     plot_hist(dist_district_wise)
 
     # just plotting graph
-    for i in range(N):
-        if g.vs(i)["state"][0] == 1:
-            g.vs(i)["color"] = 'green'
-    ig.plot(g)
+    # for i in range(N):
+    #     if g.vs(i)["state"][0] == 1:
+    #         g.vs(i)["color"] = 'green'
+    # ig.plot(g)
 
 
 if __name__ == '__main__':
