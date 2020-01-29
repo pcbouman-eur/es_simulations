148acd9fcbce588a6f6fcbbc003b4e270ef0492b
carlocampajola <c.campajola@gmail.com>
2020-01-29 14:12:03 +0100

    zealot check added

--- simulation/base.py
+++ simulation/base.py
@@ -8,11 +8,12 @@ def run_symulation(g, noise_rate, max_step, n=None):
 
     for t in range(max_step):
         node = random.randrange(1, n)
-        rnum = random.random()
-        if rnum <= noise_rate / 2.0:
-            g.vs[node]["state"] *= -1
-        elif rnum > noise_rate:
-            target_neighbor = random.sample(g.neighbors(node), 1)[0]
-            g.vs[node]["state"] = g.vs[target_neighbor]["state"]
+        if g.vs[node]["zealot"] == 0:
+            rnum = random.random()
+            if rnum <= noise_rate / 2.0:
+                g.vs[node]["state"] *= -1
+            elif rnum > noise_rate:
+                target_neighbor = random.sample(g.neighbors(node), 1)[0]
+                g.vs[node]["state"] = g.vs[target_neighbor]["state"]
 
     return g
