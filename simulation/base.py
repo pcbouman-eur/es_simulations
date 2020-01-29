1dbb088772f195a93dc09e8de3cae2a95b7f7395
tomaszraducha <raducha.tomasz@gmail.com>
2020-01-29 19:58:03 +0100

    bug fix in algorithm 2

--- simulation/base.py
+++ simulation/base.py
@@ -1,5 +1,6 @@
 import igraph as ig
 import random
+import numpy as np
 
 
 def run_symulation(g, noise_rate, max_step, n=None):
@@ -7,7 +8,7 @@ def run_symulation(g, noise_rate, max_step, n=None):
         n = len(g.vs())
 
     for t in range(max_step):
-        node = random.randrange(1, n)
+        node = np.random.randint(0, n)
         if g.vs[node]["zealot"] == 0:
             rnum = random.random()
             if rnum <= noise_rate / 2.0:
