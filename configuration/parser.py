859143397a81a3f7a5125f71ec3d619dfc10eb42
mateuszwilinski <mateuszwilinski@o2.pl>
2020-06-21 00:18:20 +0200

    fixed a bug connected with districts being different than communities

--- configuration/parser.py
+++ configuration/parser.py
@@ -53,8 +53,8 @@ parser.add_argument('-avg_deg', '--average_degree', action='store', default=5,
                     type=int, dest='avg_deg',
                     help='The average degree - defines network density.')
 
-parser.add_argument('-distr_eq_comm', '--districts_eq_communities', action='store', default=True,
-                    type=bool, dest='distr_eq_comm',
+parser.add_argument('--districts_eq_communities', action='store_const', default=False,
+                    const=True, dest='distr_eq_comm',
                     help='whether districts should be the same as communities')
 
 parser.add_argument('--reset', action='store_const', default=False, const=True, dest='reset',
